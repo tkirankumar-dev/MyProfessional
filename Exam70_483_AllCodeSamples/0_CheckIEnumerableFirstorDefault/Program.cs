0f0bd2c456291c24b7938dee29f07818fadc5669
kirankumart <kirankumart@proarch.com>
2015-04-03 12:34:52 +0530

    Check_IEnumerable_FirstOrDefault

---
+++ Exam70_483_AllCodeSamples/0_CheckIEnumerableFirstorDefault/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0_CheckIEnumerableFirstorDefault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ages = new List<int>();
+            ages.Add(10);
+            ages.Add(20);
+            ages.Add(30);
+            ages.Add(40);
+            IEnumerable<int> age_IEnumerable = (IEnumerable<int>)ages;
+            // Setting the default value to 1 after the query.
+            int firstAge = age_IEnumerable.FirstOrDefault();
+            Console.WriteLine(firstAge);
+
+            List<int> agesEmpty = new List<int>();
+
+            IEnumerable<int> agesEmpty_IEnumerable = (IEnumerable<int>)agesEmpty;
+            // Setting the default value to 1 after the query.
+            int firstAgeEmpty = agesEmpty_IEnumerable.FirstOrDefault();
+            Console.WriteLine(firstAgeEmpty);
+            Console.Read();
+        }
+    }
+}

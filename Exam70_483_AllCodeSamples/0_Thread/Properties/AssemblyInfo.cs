55cd57ce92338a2aa300bc5028e0ec071cf491f9
kirankumart <kirankumart@proarch.com>
2015-04-03 13:17:03 +0530

    Correct the Assembly Info

---
+++ Exam70_483_AllCodeSamples/0_Thread/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CheckIEnumerableFirstorDefault")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d32ed55d-3ab4-435e-805c-db33165410a4")]
+
+[assembly: InternalsVisibleTo("Exam70-483.0_Thread")]
---
+++ Exam70_483_AllCodeSamples/GlobalAssemblyInfo.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// The following attributes are applied to all the assemblies
+// in Sumo project
+
+[assembly: AssemblyCompany("Kiran Kumar T")]
+[assembly: AssemblyProduct("Personnel Stuff")]
+[assembly: AssemblyCopyright("© 2015 - Started at April 3rd 2015")]
+[assembly: AssemblyTrademark("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]

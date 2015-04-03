a351a30e4c4397771cb3a96e459e4746dd75304b
kirankumart <kirankumart@proarch.com>
2015-04-03 13:03:08 +0530

    1st Program for Exam 70-483 (Thread)

--- Exam70_483_AllCodeSamples/0_CheckIEnumerableFirstorDefault/Program.cs
+++ Exam70_483_AllCodeSamples/0_CheckIEnumerableFirstorDefault/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _0_CheckIEnumerableFirstorDefault
+namespace CheckIEnumerableFirstorDefault
 {
     class Program
     {
---
+++ Exam70_483_AllCodeSamples/0_Thread/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace ExamRef70_483
+{
+    public static class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+        public static void Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+            t.Join();
+            Console.Read();
+        }
+    }
+}
+// Displays
+//Main thread: Do some work.
+//ThreadProc: 0
+//Main thread: Do some work.
+//ThreadProc: 1
+//Main thread: Do some work.
+//ThreadProc: 2
+//Main thread: Do some work.
+//ThreadProc: 3
+//ThreadProc: 4
+//ThreadProc: 5
+//ThreadProc: 6
+//ThreadProc: 7
+//ThreadProc: 8
+//ThreadProc: 9
+//ThreadProc: 10
